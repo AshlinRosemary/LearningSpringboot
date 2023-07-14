60c09c32f15019f83785a87b3a81cbdc290f38b2
AshlinRosemary <ashlinrosemary@gmail.com>
2023-07-14 16:01:49 +0530

    Constructor based DI

--- src/main/java/com/spring/core/di/MessageSender.java
+++ src/main/java/com/spring/core/di/MessageSender.java
@@ -9,13 +9,23 @@ public class MessageSender {
 
     //private EmailService emailService;
     private MessageService messageService;
-    @Autowired
+    private MessageService smsService;
+    //@Autowired
     public MessageSender(@Qualifier("emailService") MessageService messageService){
 
         this.messageService=messageService;
+        System.out.println("Constructor based dependency Injection");
     }
+    @Autowired
+    public MessageSender(@Qualifier("emailService") MessageService messageService,
+                         MessageService smsService){
 
+        this.messageService=messageService;
+        this.smsService=smsService;
+        System.out.println("Constructor based dependency Injection 2");
+    }
     public void sendMessage(String message){
         this.messageService.sendMessage(message);
+        this.smsService.sendMessage(message);
     }
 }
