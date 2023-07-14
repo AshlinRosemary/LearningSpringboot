167754f69e6e653ce478dd543f2625cb7291f365
AshlinRosemary <ashlinrosemary@gmail.com>
2023-07-14 16:39:20 +0530

    Setter based DI for email Service

--- src/main/java/com/spring/core/di/MessageSender.java
+++ src/main/java/com/spring/core/di/MessageSender.java
@@ -9,23 +9,29 @@ public class MessageSender {
 
     //private EmailService emailService;
     private MessageService messageService;
-    private MessageService smsService;
-    //@Autowired
-    public MessageSender(@Qualifier("emailService") MessageService messageService){
-
-        this.messageService=messageService;
-        System.out.println("Constructor based dependency Injection");
-    }
     @Autowired
-    public MessageSender(@Qualifier("emailService") MessageService messageService,
-                         MessageService smsService){
-
-        this.messageService=messageService;
-        this.smsService=smsService;
-        System.out.println("Constructor based dependency Injection 2");
+    public void setMessageService(@Qualifier("emailService") MessageService messageService) {
+        this.messageService = messageService;
+        System.out.println("Setter based Injection for Email Service");
     }
+
+    /*private MessageService smsService;
+        //@Autowired
+        public MessageSender(@Qualifier("emailService") MessageService messageService){
+
+            this.messageService=messageService;
+            System.out.println("Constructor based dependency Injection");
+        }
+        @Autowired
+        public MessageSender(@Qualifier("emailService") MessageService messageService,
+                             MessageService smsService){
+
+            this.messageService=messageService;
+            this.smsService=smsService;
+            System.out.println("Constructor based dependency Injection 2");
+        }*/
     public void sendMessage(String message){
         this.messageService.sendMessage(message);
-        this.smsService.sendMessage(message);
+        //this.smsService.sendMessage(message);
     }
 }
