78f08b100b515f3afdca5d675e6fe6084233a1c0
AshlinRosemary <ashlinrosemary@gmail.com>
2023-07-15 13:10:37 +0530

    Setter DI -->To stop circular dependency

--- src/main/java/com/spring/core/di/Demo.java
+++ src/main/java/com/spring/core/di/Demo.java
@@ -1,16 +1,24 @@
 package com.spring.core.di;
 class DependencyA{
+    public void setDependencyB(DependencyB dependencyB) {
+        this.dependencyB = dependencyB;
+    }
 
+    private DependencyB dependencyB;
 }
 class DependencyB{
+    private DependencyA dependencyA;
 
+    public void setDependencyA(DependencyA dependencyA) {
+        this.dependencyA = dependencyA;
+    }
 }
 class DependencyC{
 
 }
 public class Demo {
-    private DependencyA dependencyA;
-    private DependencyB dependencyB;
+
+
     private DependencyC dependencyC;
 /* Constructor DI --> Mandatory DI---> becoz During bean creation itself DI takes place
     public Demo(DependencyA dependencyA, DependencyB dependencyB, DependencyC dependencyC) {
@@ -18,7 +26,7 @@ public class Demo {
         this.dependencyB = dependencyB;
         this.dependencyC = dependencyC;
     }*/
-
+/*Eg for Setter Injection
     public void setDependencyA(DependencyA dependencyA) {
         this.dependencyA = dependencyA;
     }
@@ -29,5 +37,5 @@ public class Demo {
 
     public void setDependencyC(DependencyC dependencyC) {
         this.dependencyC = dependencyC;
-    }
+    }*/
 }
